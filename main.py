7d40b90becd9586b59c790c6680996ab4e2ea766
Taka005 <01taka005@gmail.com>
2023-10-15 11:38:31 +0000

    完成

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import sympy as sp
 import numpy as np
+from flask import Flask, request, send_file
+import io
 
-x = sp.Symbol("x")
+app = Flask(__name__)
 
-img = sp.plotting.plot("x**3","x",(x,-2,2),legend=True)
-img.save("graph.png")
+@app.route("/", methods=["GET"])
+def main():
+    formula = request.args.get("formula").split(",")
+
+    x = sp.Symbol("x")
+
+    if len(formula) == 1:
+        img = sp.plotting.plot(formula[0],(x,-3,3),legend=True)
+    elif len(formula) == 2:
+        img = sp.plotting.plot(formula[0],formula[1],(x,-3,3),legend=True)
+    elif len(formula) == 3:
+        img = sp.plotting.plot(formula[0],formula[1],formula[2],(x,-3,3),legend=True)
+
+    file = io.BytesIO()
+    img.save(file)
+    file.seek(0)
+    return send_file(file,mimetype="image/png")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=4000)
